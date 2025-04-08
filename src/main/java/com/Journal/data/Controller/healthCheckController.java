1eae4f564af72e7e0b8c131e53fe82f3a02c9f2a
Neha <nehachoubeyggn@gmail.com>
2025-04-09 00:02:12 +0530

    first changes.

---
+++ src/main/java/com/Journal/data/Controller/healthCheckController.java
@@ -0,0 +1,13 @@
+package com.Journal.data.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class healthCheckController {
+
+	@GetMapping("/health-Check")
+	public String healthCheck() {
+		return "OKAYDATA";
+		
+	}
+}
